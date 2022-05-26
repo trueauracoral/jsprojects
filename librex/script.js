b138d7937b0d62417ad51f9ec748955cf425dc43
zortazert <zortazert@matthewevan.xyz>
2022-05-26 15:51:59 -0500

    CODE ACTUALLY WORK NOW!

--- librex/script.js
+++ librex/script.js
@@ -10,17 +10,19 @@ function getSearchResults(e) {
 	fetch(librex_instance+"api.php?q="+name+"&p=1&type=0")
 	.then((response) => response.json())
 	.then((data) => {
-		document.querySelector(".searchResult").innerHTML = `
-		<p>data[0].url</p>
+			let list = `
+			
+			`;
+		data.forEach(function(result){
+			list += `
+		<div class="searchResultInfo">
+			<p style="color:#ADADAD">${result.base_url}</p>
+			<a href="${result.url}"><h2>${result.title}</h2></a>
+			<p>${result.description}</p>
+		</div>
 		`;
-		//data.forEach(function(result){
-		//	list += `
-		//<div class="searchResultInfo">
-		//	<p>${result.url}</p>
-		//	<a href="${result.url}">${result.title}</a>
-		//</div>
-		//`;
-		//})
+		})
+		document.querySelector(".searchResult").innerHTML = list
 	})
 	.catch((err) => {
 		document.querySelector(".searchResult").innerHTML = `
