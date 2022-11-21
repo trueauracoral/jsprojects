b609e4b0a31d778ae9d7d57cfcd88edead954a04
zortazert <zortazert@matthewevan.xyz>
2022-11-20 22:13:32 -0600

    Ability to go to multiple boards

--- chan/script.js
+++ chan/script.js
@@ -1,46 +1,84 @@
-function title(stuff){
-    var title = `${stuff.name} ${stuff.now} No.${stuff.no}`;
-    //if (stuff.includes("sub")){
-    //    title = `${stuff.sub} | ${title}`;
-    //}
+function title(stuff) {
+    var title = `<span style="color:#D48D3D">${stuff.name}</span> ${stuff.now} No.${stuff.no}`;
+    if (stuff.hasOwnProperty("sub")) {
+        title = `${stuff.sub} | ${title}`;
+    }
     return title;
 }
 
-const board = "g";
-
-fetch(`https://cors-anywhere.herokuapp.com/https://a.4cdn.org/${board}/catalog.json`)
-.then((response) => response.json())
-.then((data) => {
-    console.log(data[0].threads)
-	let list = `
-
-	`;
-	data[0].threads.forEach(function(result){
-		list += `
-	<hr>
-	<img style='vertical-align:middle;' src="https://i.4cdn.org/${board}/${result.tim}${result.ext}" alt="${result.no}"/>
-	<div style='vertical-align:middle; display:inline-block;'class="pokemonInfo">
-	<div class="Thread">
-		<p style="color:#ADADAD">${title(result)}</p>
-		<p>${result.com}</p>
-	</div>
-	`;
-	    if (result.replies != 0) {
-	    result.last_replies.forEach(function(reply){
-		list += `
-	<div class="Reply">
-	<p style="color:#ADADAD">>>${title(reply)}</p>
-	<p>${reply.com}</p>
-	`;
-	    })
-	    }
-	});
-    document.querySelector(".threads").innerHTML = list;
-})
-.catch((err) => {
-	document.querySelector(".threads").innerHTML = `
+const board = "vp";
+boardButtons()
+function boardButtons() {
+    fetch(`https://cors-anywhere.herokuapp.com/http://a.4cdn.org/boards.json`, {
+        method: "GET",
+        headers: {
+            'Accept': 'application/json',
+        }
+    })
+    .then((response) => response.json())
+    .then((data) => {
+        let list = `<p>[`
+        data.boards.forEach(function (result) {
+            list += `<a onclick="getBoard('${result.board}')" href="#">${result.board}</a> `
+        })
+        document.querySelector(".boards").innerHTML = list + "]</p>"
+    })
+    .catch((err) => {
+        document.querySelector(".threads").innerHTML = `
 <div>
 <p>Could not find this 😟. Try again.</p>
 </div>`;
-    console.log(err);
-});
+        console.log(err);
+    });
+}
+function getBoard(board) {
+    fetch(`https://cors-anywhere.herokuapp.com/http://a.4cdn.org/${board}/catalog.json`, {
+        method: "GET",
+        headers: {
+            'Accept': 'application/json',
+        }
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            let list = `
+
+	`;
+            data[0].threads.forEach(function (result) {
+                list += `
+	<hr>
+	<div class="Thread" style='vertical-align:middle; display:inline-block;'>
+    <p style="color:#ADADAD">${title(result)}</p>
+	<img src="https://i.4cdn.org/${board}/${result.tim}${result.ext}" alt="${result.no}" style='vertical-align:middle;'/>
+    <p>${result.com}</p>
+	</div>
+	`;
+                if (result.replies != 0) {
+                    result.last_replies.forEach(function (reply) {
+                        list += `
+                <div class="Reply" style='background-color: #2C2A2A;border:3px solid;'>
+                <p style="color:#ADADAD">>>${title(reply)}</p>
+                `;
+                        if (reply.hasOwnProperty("filename")) {
+                            list += `
+	<img style='vertical-align:middle;' src="https://i.4cdn.org/${board}/${reply.tim}${reply.ext}" alt="${result.no}"/>
+	`;
+                        }
+                        if (reply.hasOwnProperty("com")) {
+                            list += `
+	                <p>${reply.com}</p>
+	                `;
+                        }
+                        list += '</div>'
+                    })
+                }
+            });
+            document.querySelector(".threads").innerHTML = list;
+        })
+        .catch((err) => {
+            document.querySelector(".threads").innerHTML = `
+<div>
+<p>Could not find this 😟. Try again.</p>
+</div>`;
+            console.log(err);
+        });
+}
